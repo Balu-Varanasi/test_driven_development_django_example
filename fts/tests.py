7e24f1557f699245cc7f4cf099dc242651296613
Bala Subrahmanyam Varanasi <balu@agiliq.com>
2013-03-28 17:28:42 +0530

    added first FT

--- fts/tests.py
+++ fts/tests.py
@@ -5,12 +5,26 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
 
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PollsTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = WebDriver()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_create_new_poll_via_admin_site(self):
+        # Gertrude opens her web browser, and goes to the admin page
+        self.browser.get(self.live_server_url + '/admin/')
+
+        # She sees the familiar 'Django administration' heading
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+
+        # TODO: use the admin site to create a Poll
+        self.fail('finish this test')
