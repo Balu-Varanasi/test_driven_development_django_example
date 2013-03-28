12ed273b040a5f83f5cc6f2436a9bc506d3e0c39
Bala Subrahmanyam Varanasi <balu@agiliq.com>
2013-03-28 18:43:36 +0530

    added some more assertIns to check 'username' and 'password'

--- fts/tests.py
+++ fts/tests.py
@@ -9,7 +9,11 @@ Replace this with more appropriate tests for your application.
 from django.test import LiveServerTestCase
 from selenium.webdriver.firefox.webdriver import WebDriver
 
+from selenium.webdriver.common.keys import Keys
+
 class PollsTest(LiveServerTestCase):
+
+    fixtures = ['admin_user.json']
 
     def setUp(self):
         self.browser = WebDriver()
@@ -24,7 +28,25 @@ class PollsTest(LiveServerTestCase):
 
         # She sees the familiar 'Django administration' heading
         body = self.browser.find_element_by_tag_name('body')
-        self.assertIn('Django administration', body.text)
+        self.assertIn('Django administration', \
+                          body.find_element_by_id("header").text)
 
-        # TODO: use the admin site to create a Poll
-        self.fail('finish this test')
+        # She types in her username and passwords and hits return
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('adm1n')
+        password_field.send_keys(Keys.RETURN)
+
+        # her username and password are accepted, and she is taken to
+        # the Site Administration page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        # She now sees a couple of hyperlink that says "Polls"
+        polls_links = self.browser.find_elements_by_link_text('Polls')
+        self.assertEquals(len(polls_links), 2)
+
+        # TODO: Gertrude uses the admin site to create a new Poll
+        self.fail('todo: finish tests')
